c52d8471e458cdc627d26524325e40b65e53d641
f4hagen <ffhagen@gmail.com>
2020-02-26 01:25:59 -0500

    fixing formatting of last screen

--- BibleProject/src/components/step4.js
+++ BibleProject/src/components/step4.js
@@ -19,7 +19,7 @@ export default class Step4 extends React.Component {
     console.log(this.props.navigation.state.params.pic);
     return (
       <View style={{flex: 1, backgroundColor: '#95adbe'}}>
-        <TouchableOpacity
+        <View
           style={{
             flex: 4,
             justifyContent: 'center',
@@ -34,23 +34,28 @@ export default class Step4 extends React.Component {
               padding: 20,
               marginLeft: 20,
               marginRight: 20,
+              marginBottom: 30,
             }}>
             {' '}
             Step 4: Finsihed{' '}
           </Text>
-          <Text style={{paddingTop: 40, fontSize: 20, textAlign: 'center'}}>
+          <Text style={{paddingTop: 40, fontSize: 22, textAlign: 'center'}}>
             Save your image to share and send{' '}
           </Text>
-          <Text style={{paddingTop: 10, fontSize: 20, textAlign: 'center'}}>
+          <Text style={{paddingTop: 10, fontSize: 22, textAlign: 'center'}}>
             or save as a new screensaver{' '}
           </Text>
+        </View>
+        <TouchableOpacity>
           <Text
             style={{
               fontSize: 25,
               backgroundColor: '#465881',
+              alignSelf: 'center',
               textAlign: 'center',
               borderWidth: 1,
               padding: 20,
+              marginBottom: 120,
               marginLeft: 20,
               marginRight: 20,
             }}>
